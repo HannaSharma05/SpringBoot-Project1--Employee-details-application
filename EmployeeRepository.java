c61aee619815060faf2608c9b72990d6f27b479e
Hanna <150518039+HannaSharma05@users.noreply.github.com>
2026-07-02 14:02:49 +0530

    Change Employee ID type from Long to Integer

--- EmployeeRepository.java
+++ EmployeeRepository.java
@@ -3,11 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // Optional: list employees by designation
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findByDesignation(String designation);
 }
